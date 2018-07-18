76c0fe3d87cf06423d94d96387daf25cf598fdc1
marroco93 <20110736@aloe.ulima.edu.pe>
2018-07-18 11:10:43 -0500

    no message

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -768,31 +768,7 @@
 			<div class="section-padding"></div>
 		</div><!-- Shop / -->
 		
-		<!-- Offer -->
-		<div class="offer container-fluid no-padding">
-			<div class="offer-shape">
-				<svg width="100%" height="100%">
-					<clippath id="clipPolygon1" clippathunits="objectBoundingBox">
-						<polygon points="0 0, 32 100, 100 100, 100 0"></polygon>
-					</clippath>
-				</svg>
-			</div>
-			
-			<!-- Container -->
-			<div class="container">
-				<div class="row">
-					<div class="col-md-5 col-sm-6">
-						<div class="smart-price">
-							<h3>Something Special For Your Pets</h3>
-							<h2>In Smart Price</h2>
-						</div>
-					</div>
-					<div class="col-md-4 col-sm-6 pull-right offer-box">					
-						<h2>30%</h2><h3>Upto 500 Products<span>On Season Offer Sale</span></h3>
-					</div>
-				</div>
-			</div><!-- Container /- -->
-		</div><!-- Offer / -->
+
 		
 		<!-- Blog -->
 		<div id="blog-section" class="blog-section container-fluid no-padding">
